3c71176f061db3807e5d9a25af7627be97e0b81c
adithya-6337 <veldandiadithya337@gmail.com>
2022-11-04 13:35:03 +0530

    Appending

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,10 +2,10 @@ package com.bridgelabz;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.push(70);
-        linkedList.push(30);
-        linkedList.push(56);
-        linkedList.display();
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.add(56);
+        linkedList1.append(30);
+        linkedList1.append(70);
+        linkedList1.display();
     }
 }
